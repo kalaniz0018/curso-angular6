c5e8d5d04b28270934db6f4fa696ee10ba970e0a
KAlanizK <karinaalaniz1994@gmail.com>
2020-11-10 11:51:09 -0300

    Principios de REST

--- angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.ts
+++ angular-wishlist/src/app/components/destino-detalle/destino-detalle.component.ts
@@ -1,48 +1,13 @@
-import { Component, OnInit, InjectionToken, Inject, Injectable } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { DestinosApiClient } from './../../models/destinos-api-client.model';
 import { DestinoViaje } from './../../models/destino-viaje.model';
 import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { AppState } from 'src/app/app.module';
-
-class DestinosApiClientViejo{
-  getById(id: string): DestinoViaje{
-    console.log('Llamando por la clase vieja! ');
-    return null;
-  }
-}
-
-interface AppConfig{
-  apiEndpoint: String;
-}
-const APP_CONFIG_VALUE: AppConfig = {
-  apiEndpoint: 'mi_api.com'
-};
-
-const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
-
-@Injectable()
-class DestinosApiClientDecorated extends DestinosApiClient{
-  constructor(@Inject(APP_CONFIG) private config: AppConfig, store: Store<AppState>){
-    super(store);
-  }
-  getById(id: String): DestinoViaje{
-    console.log('Llamado por la clase decorada!');
-    console.log('config: '+ this.config.apiEndpoint);
-    return super.getById(id);
-    }
-  }
 
 @Component({
   selector: 'app-destino-detalle',
   templateUrl: './destino-detalle.component.html',
   styleUrls: ['./destino-detalle.component.css'],
-  providers: [
-    DestinosApiClient,
-   {provide: APP_CONFIG, useValue: APP_CONFIG_VALUE},
-   {provide: DestinosApiClientViejo, useClass: DestinosApiClientDecorated},
-   {provide: DestinosApiClientViejo, useExisting: DestinosApiClient}
-  ]
+  providers: [ DestinosApiClient]  
 })
 export class DestinoDetalleComponent implements OnInit {
   destino: DestinoViaje;
@@ -65,7 +30,7 @@ export class DestinoDetalleComponent implements OnInit {
     }]
   };
 
-  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClientViejo) {}
+  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClient) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
--- angular-wishlist/src/app/models/destinos-api-client.model.ts
+++ angular-wishlist/src/app/models/destinos-api-client.model.ts
@@ -1,23 +1,56 @@
-import { analyzeAndValidateNgModules } from '@angular/compiler';
-import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { BehaviorSubject, Subject } from 'rxjs';
-import { AppState } from '../app.module';
+import {Injectable, Inject, forwardRef} from '@angular/core';
 import { DestinoViaje } from './destino-viaje.model';
-import { ElegidoFavoritoAction, NuevoDestinoAction } from './destinos-viajes-state.model';
-
+import { Store } from '@ngrx/store';
+import {
+    NuevoDestinoAction,
+    ElegidoFavoritoAction
+  } from './destinos-viajes-state.model';
+import {AppConfig, AppState, APP_CONFIG} from './../app.module';
+import { HttpClient, HttpClientModule, HttpHeaders, HttpRequest, HttpResponse } from '@angular/common/http';
 
 @Injectable()
 export class DestinosApiClient {
-	constructor(private store: Store<AppState>) {
-	}
-	add(d: DestinoViaje){
-		this.store.dispatch(new NuevoDestinoAction(d))
-	}
-	elegir(d: DestinoViaje){
-		this.store.dispatch(new ElegidoFavoritoAction(d))
-	}
-	getById(id: String){
-		return new DestinoViaje("","");
-	}
+  destinos: DestinoViaje[] = [];
+
+  constructor(
+    private store: Store<AppState>,
+    @Inject(forwardRef(() => APP_CONFIG)) private config: AppConfig,
+    private http: HttpClient
+  ) {
+    this.store
+      .select(state => state.destinos)
+      .subscribe((data) => {
+        console.log('destinos sub store');
+        console.log(data);
+        this.destinos = data.items;
+      });
+    this.store
+      .subscribe((data) => {
+        console.log('all store');
+        console.log(data);
+      });
+  }
+
+  add(d: DestinoViaje) {
+    const headers: HttpHeaders = new HttpHeaders({'X-API-TOKEN': 'token-seguridad'});
+    const req = new HttpRequest('POST', this.config.apiEndpoint + '/my', { nuevo: d.nombre }, { headers: headers });
+    this.http.request(req).subscribe((data: HttpResponse<{}>) => {
+      if (data.status === 200) {
+        this.store.dispatch(new NuevoDestinoAction(d));
+	  }
+    });
+  }
+
+  getById(id: String): DestinoViaje {
+    return this.destinos.filter(function(d) { return d.id.toString() === id; })[0];
+  }
+
+  getAll(): DestinoViaje[] {
+    return this.destinos;
+  }
+
+  elegir(d: DestinoViaje) {
+    // aqui incovariamos al servidor
+    this.store.dispatch(new ElegidoFavoritoAction(d));
+  }
 }
